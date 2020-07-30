101952cfe09a8f52deabeda72c05a6c0679543b7
jamiecwebber <jamiecwebber@gmail.com>
2020-07-30 16:19:17 -0400

    got styled-components working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header.js';
+// import { StyledHeader } from './components/Header.js';
+import styled from 'styled-components'
+
+const Header = ({ className }) => {
+  return (
+      <header className={className}>
+          <div>
+              JAMIE CHRISTOPHER WEBBER
+          </div>
+          <nav>
+              <div>works</div>
+              <div>tools</div>
+              <div>blog</div>
+              <div>contact</div>
+          </nav>
+      </header>
+  )
+};
+
+const StyledHeader = styled(Header)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: right;
+  color: palevioletred;
+`;
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <StyledHeader />
+      <Title>BLAH</Title>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,6 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function Header() {
-    return (
-        <header>
-            <div>
-                JAMIE CHRISTOPHER WEBBER
-            </div>
-            <nav>
-                <div>works</div>
-                <div>tools</div>
-                <div>blog</div>
-                <div>contact</div>
-            </nav>
-        </header>
-    )
-    
-};
 
-export default Header;
+
+export { Header, StyledHeader };
