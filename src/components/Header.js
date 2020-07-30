92bf8c5673a152fc3eb70dbd0e9a940501ac8cf7
jamiecwebber <jamiecwebber@gmail.com>
2020-07-30 15:45:22 -0400

    added basic header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 function Header() {
-    return <div>BLAH</div>
+    return (
+        <header>
+            <div>
+                JAMIE CHRISTOPHER WEBBER
+            </div>
+            <nav>
+                <div>works</div>
+                <div>tools</div>
+                <div>blog</div>
+                <div>contact</div>
+            </nav>
+        </header>
+    )
+    
 };
 
 export default Header;
